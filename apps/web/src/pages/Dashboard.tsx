54cc8f79d1914b161c22253fb1a58c9ea17fcf13
Serverless <serverless25@gmail.com>
2023-09-26 18:28:59 -0400

    create skeleton with sidebar for dashboard

--- apps/web/src/pages/Dashboard.tsx
+++ apps/web/src/pages/Dashboard.tsx
@@ -3,6 +3,13 @@ import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { getAuth, signInWithCustomToken } from "firebase/auth";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/UserContext";
+import {
+  MdDashboard,
+  MdMenu,
+  MdPerson,
+  MdShoppingCart,
+  MdSubscriptions,
+} from "react-icons/md";
 import { trpc } from "../trpc";
 
 const SignIn = () => {
@@ -35,7 +42,6 @@ const SignIn = () => {
           const signed = await wallet.signMessage!(
             new window.TextEncoder().encode(message)
           );
-          console.log(signed);
           setSignature(signed);
         } catch (e) {
           console.log(e);
@@ -64,28 +70,109 @@ const SignIn = () => {
   );
 };
 
-const Navbar = () => {
-  return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="bold  cursor-pointer normal-case text-2xl">SolSubs</a>
-      </div>
-      <div className="flex-none">
-        <WalletMultiButton />
-      </div>
-    </div>
-  );
-};
-
 export const DashboardPage = () => {
   const wallet = useWallet();
   const { user } = useContext(UserContext);
+  const sidebarOptions = [
+    {
+      label: "Dashboard",
+      icon: <MdDashboard className="h-8 w-8 text-primary" />,
+    },
+    {
+      label: "Plans",
+      icon: <MdShoppingCart className="h-8 w-8 text-primary" />,
+    },
+    {
+      label: "Subscriptions",
+      icon: <MdSubscriptions className="h-8 w-8 text-primary" />,
+    },
+    {
+      label: "Subscribers",
+      icon: <MdPerson className="h-8 w-8 text-primary" />,
+    },
+  ];
+  const [activeSidebarOption, setActiveSidebarOption] = useState(
+    sidebarOptions[0]
+  );
+
+  const closeSidebar = () => {
+    document.getElementById("my-drawer")?.click();
+  };
 
   return (
-    <div>
-      <Navbar />
+    <div className="h-full">
       {(!wallet.connected || !user) && <SignIn />}
-      {wallet.connected && <div></div>}
+      {wallet.connected && !!user && (
+        <div className="w-full h-full flex flex-row drawer">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="absolute hidden md:flex right-[25px] top-[25px]">
+            <WalletMultiButton />
+          </div>
+          <div className="md:hidden flex flex-row mt-[25px] w-full justify-evenly">
+            <div className="flex flex-col gap-4">
+              <div className="cursor-pointer flex gap-2 flex-row">
+                <img
+                  src="/solsubs_logo.png"
+                  alt="SolSubs logo"
+                  className="w-12 h-12"
+                />
+                <p className="bold normal-case text-3xl">SolSubs</p>
+              </div>
+              <label htmlFor="my-drawer" className="btn btn-square btn-ghost">
+                <MdMenu className="h-12 w-12 text-white" />
+              </label>
+            </div>
+            <WalletMultiButton />
+          </div>
+          <div className="drawer-side">
+            <label htmlFor="my-drawer" className="drawer-overlay"></label>
+            <div className="flex flex-col gap-4 bg-slate-800 h-full">
+              {sidebarOptions.map((option) => (
+                <div
+                  className={`flex flex-row gap-2 hover:bg-slate-600 py-10 rounded cursor-pointer px-4 ${
+                    option.label === activeSidebarOption.label
+                      ? "bg-slate-600"
+                      : ""
+                  }`}
+                  key={option.label}
+                  onClick={() => {
+                    setActiveSidebarOption(option);
+                    closeSidebar();
+                  }}
+                >
+                  {option.icon}
+                  <p className="font-bold text-2xl">{option.label}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex-1 max-w-[275px] bg-gray-800 hidden md:flex flex-col h-[100vh]  ">
+            <div className="cursor-pointer flex flex-row gap-4 py-10  px-4">
+              <img
+                src="/solsubs_logo.png"
+                alt="SolSubs logo"
+                className="w-12 h-12 z-[2]"
+              />
+              <p className="bold normal-case text-2xl">SolSubs</p>
+            </div>
+            {sidebarOptions.map((option) => (
+              <div
+                className={`flex flex-row gap-2 hover:bg-slate-600 py-10 rounded cursor-pointer px-4 ${
+                  option.label === activeSidebarOption.label
+                    ? "bg-slate-600"
+                    : ""
+                }`}
+                key={option.label}
+                onClick={() => setActiveSidebarOption(option)}
+              >
+                {option.icon}
+                <p className="font-bold text-2xl">{option.label}</p>
+              </div>
+            ))}
+          </div>
+          <div className="drawer-content flex flex-col"></div>
+        </div>
+      )}
     </div>
   );
 };
